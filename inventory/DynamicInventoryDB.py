f886f87e855994610db5f1dbed4b368c7df2f1ea
leeyan85 <leeyan85@gmail.com>
2017-09-03 19:23:12 +0800

    modify dos format to unix

--- inventory/DynamicInventoryDB.py
+++ inventory/DynamicInventoryDB.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python 
+#!/usr/bin/env python
 import os
 import sys
 import argparse
@@ -56,5 +56,31 @@ class Inventory(object):
         return inventory
     
 
+
+def example_inventory():
+    session=get_session()
+    #groups=session.query(Group).filter(Group.name=='webserver').all()
+    groups=session.query(Group).all()
+    inventory={'_meta':{'hostvars':{}}}
+    for group in groups:
+        hosts=[host.IP for host in group.host_list]
+        vars={ 
+                'ansible_become':True,
+                'ansible_become_method':'su',
+                'ansible_become_user':group.username,
+                'ansible_become_pass':group.password,
+            }
+        inventory[group.name]={
+                'hosts':hosts,
+                'vars':vars,
+        }
+        for host in group.host_list:
+            inventory['_meta']['hostvars'][host.IP]={
+                        'software':host.software,
+            }
+    print json.dumps(inventory,indent=4)
+    #return json.dumps(inventory,indent=4)
+
+#example_inventory()
 
 Inventory()
