8a9e8513add7623923d62d8d1599d035aad4669e
leeyan85 <leeyan85@gmail.com>
2017-09-10 11:56:12 +0800

    modify plugin example

--- plugin/callback_write_logging.py
+++ plugin/callback_write_logging.py
@@ -4,11 +4,6 @@ from ansible.plugins.callback import CallbackBase
 import json
 import time
 import logging
-import redis
-
-def set_redis_connection():
-    redis_cli=redis.Redis(host='127.0.0.1',port=6379,db=0)
-    return redis_cli
 
 def display_result_info(result):
     print json.dumps(dir(result._host),indent=4)
@@ -39,16 +34,17 @@ class CallbackModule(CallbackBase): #callback plugin
 
     def v2_runner_on_unreachable(self, result):
         self.host_unreachable[result._host.get_name()] = result
-        #display_result_info(result)
 
     def v2_runner_on_ok(self, result, *args, **kwargs):  
         self.host_ok.append({result._host.get_name():result})
-        #display_result_info(result)
-        print result._host.get_name(),result._task.name,self.playstarttime,self.taskstarttime,endtime   
+        endtime=time.time()
+        print endtime
+        info="%s %s %s %s %s"%(result._host.get_name(),result._task.name,self.playstarttime,self.taskstarttime,endtime)
+        print info
+        write_task_info_to_log(info)
         
     def v2_runner_on_failed(self, result,  *args, **kwargs):  
         self.host_failed[result._host.get_name()] = result
-        #display_result_info(result)
 
     def v2_playbook_on_play_start(self, play):
         self.playstarttime=time.time()
@@ -63,7 +59,3 @@ class CallbackModule(CallbackBase): #callback plugin
     def v2_playbook_on_task_start(self, task, is_conditional):
         self.taskstarttime=time.time()
 
-if __name__=="__main__":
-    write_result_to_redis()
-    
-    
--- plugin/callback_write_redis.py
+++ plugin/callback_write_redis.py
@@ -1,76 +1,44 @@
 #!/bin/env python
 # -*- coding:utf8 -*-
-from ansible.plugins.callback import CallbackBase
+import os
+import sys
 import json
 import time
+from ansible.plugins.callback import CallbackBase
 import redis
-
+   
 def set_redis_connection():
     redis_cli=redis.Redis(host='127.0.0.1',port=6379,db=0)
     return redis_cli
 
-def display_result_info(result):
-    print json.dumps(dir(result._host),indent=4)
-    print json.dumps(dir(result._result),indent=4)
-    print json.dumps(dir(result._task),indent=4)
 
 def write_task_info_to_redis(result):
     redis_cli=set_redis_connection()
-    for host_results in result:
-        for key in host_results.keys():        
-            print key,host_results[key]._result
-            redis_cli.rpush(key,host_results[key]._result)
-            
+    for key in result.keys():        
+        print key,result[key]._result
+        redis_cli.rpush(key,result[key]._task)    
 
-    
-class CallbackModule(CallbackBase): #callback plugin
+            
+class CallbackModule(CallbackBase):
     def __init__(self, *args, **kwargs):  
         super(CallbackModule, self).__init__(*args, **kwargs)  
-        self.host_ok = []
+        self.host_ok = {}  
         self.host_unreachable = {}  
         self.host_failed = {}
-        self.task_ok={}
-        self.playstarttime='play start time'
-        self.taskstarttime='task start time'
 
     def v2_runner_on_unreachable(self, result):  
         self.host_unreachable[result._host.get_name()] = result
-        #display_result_info(result)
-
-    def v2_runner_on_ok(self, result, *args, **kwargs):  
-        self.host_ok.append({result._host.get_name():result})
         display_result_info(result)
 
-
+    def v2_runner_on_ok(self, result,  *args, **kwargs):  
+        self.host_ok[result._host.get_name()] = result  
+        write_task_info_to_redis(self.host_ok)
     def v2_runner_on_failed(self, result,  *args, **kwargs):  
         self.host_failed[result._host.get_name()] = result
-        #display_result_info(result)
+        display_result_info(result)
 
     def v2_playbook_on_play_start(self, play):
         self.playstarttime=time.time()
         
     def v2_playbook_on_task_start(self, task, is_conditional):
         self.taskstarttime=time.time()
-
-
-    def v2_runner_on_ok(self, result, *args, **kwargs):  
-        self.host_ok.append({result._host.get_name():result})
-        #display_result_info(result)
-        endtime=time.time()
-        #print result._host.get_name(),result._task.name,self.playstarttime,self.taskstarttime,endtime   
-        
-
-    def v2_runner_on_failed(self, result,  *args, **kwargs):  
-        self.host_failed[result._host.get_name()] = result
-        #display_result_info(result)
-
-    def v2_playbook_on_play_start(self, play):
-        self.playstarttime=time.time()
-        
-    def v2_playbook_on_task_start(self, task, is_conditional):
-        self.taskstarttime=time.time()
-
-if __name__=="__main__":
-    write_result_to_redis()
-    
-    
